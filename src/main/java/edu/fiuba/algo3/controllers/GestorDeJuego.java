fc8556e7c38da5847a131acd7a77b4c03ca6a64d
Lucia <lpovis@fi.uba.ar>
2025-11-06 16:54:52 -0300

    agrego archivo maven y package siguiendo el modelo que da la catedra

---
+++ src/main/java/edu/fiuba/algo3/controllers/GestorDeJuego.java
@@ -0,0 +1,4 @@
+package edu.fiuba.algo3.controllers;
+//controladores -> manejo de interacciones
+public class GestorDeJuego {
+}
---
+++ src/main/java/edu/fiuba/algo3/modelo/Jugador.java
@@ -0,0 +1,4 @@
+package edu.fiuba.algo3.modelo;
+//modelo -> logica del juego
+public class Jugador {
+}
