7098f49cba870401d1441da4f40155f31291e27c
gianfranco turco <gianfrancoturco@live.com>
2025-11-16 13:48:46 -0300

    Agrego Entrega1 test

---
+++ src/main/test/edu/fiuba/algo3/entrega_1/Entrega1Test.java
@@ -0,0 +1,46 @@
+package edu.fiuba.algo3.entrega_1;
+
+import edu.fiuba.algo3.modelo.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import java.util.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+public class Entrega1Test {
+
+    @Test
+    public void seCreaTableroConJugadoresSeValidaLaReglaDeLaDistanciaEnCasilleroConPobladoYTiraExcepcion() {
+
+        Tablero tablero = Tablero.getInstance();
+        tablero.crearGrafo();
+
+        Jugador luis = new Jugador("Luis");
+        Jugador federico = new Jugador("Federico");
+
+        luis.colocarPiezaInicial("poblado", new ArrayList<>(List.of(1)));
+        luis.colocarPiezaInicial("camino", new ArrayList<>(List.of(1, 2)));
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            federico.colocarPiezaInicial("poblado", new ArrayList<>(List.of(1)));;
+        });
+    }
+
+    @Test
+    public void seCreaTableroConJugadoresSeValidaLaReglaDeLaDistanciaEnCasilleroConAdyacenteAlPobladoYTiraExcepcion() {
+
+        Tablero tablero = Tablero.getInstance();
+        tablero.crearGrafo();
+
+        Jugador luis = new Jugador("Luis");
+        Jugador federico = new Jugador("Federico");
+
+        luis.colocarPiezaInicial("poblado", new ArrayList<>(List.of(1)));
+        luis.colocarPiezaInicial("camino", new ArrayList<>(List.of(1, 2)));
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            federico.colocarPiezaInicial("poblado", new ArrayList<>(List.of(2)));;
+        });
+    }
+}
