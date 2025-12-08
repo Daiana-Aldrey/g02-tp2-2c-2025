10f9a38ba622d4e86493fe2cf870a76d6f1071da
gianfranco turco <gianfrancoturco@live.com>
2025-12-08 18:34:00 -0300

    Cambio VistaArista

--- src/main/java/edu/fiuba/algo3/vistas/VistaArista.java
+++ src/main/java/edu/fiuba/algo3/vistas/VistaArista.java
@@ -12,6 +12,8 @@ import javafx.scene.paint.Color;
 public class VistaArista extends Button {
     private final Arista arista;
     private ControladorArista controlador;
+    private Circle circulo;
+    private Line linea;
 
     public VistaArista(Arista arista) {
         this.arista = arista;
@@ -20,24 +22,32 @@ public class VistaArista extends Button {
         double radius = 20;
         double diameter = radius * 2;
 
-        Circle circulo = new Circle(radius);
+        circulo = new Circle(radius);
         circulo.setFill(Color.BEIGE);
         circulo.setStroke(Color.BLACK);
+
+        // --- LÍNEA PARA CUANDO HAY CAMINO ---
+        linea = new Line(0, 0, 40, 0);
+        linea.setStrokeWidth(8);
+        linea.setStroke(arista.obtenerPieza().obtenerJugador().obtenerColor());  // luego podés hacer que sea color del jugador
 
         setPrefSize(diameter, diameter);
         setMinSize(diameter, diameter);
         setMaxSize(diameter, diameter);
 
         setShape(circulo);
+        setGraphic(circulo);
         setVisible(false);
     }
 
 
     public void actualizar() {
         if (arista.estaDisponible()) {
+            setGraphic(circulo);
             setVisible(true);
         } else {
-            setVisible(false);
+            setGraphic(linea);
+            setVisible(true);
         }
     }
 }
