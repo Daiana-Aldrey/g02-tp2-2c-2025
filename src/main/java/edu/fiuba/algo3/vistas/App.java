61bfe9db659d092562d9bded26f28b018b1c0b2d
daiana <daldrey@fi.uba.ar>
2025-12-11 02:55:18 -0300

    borro harcodeo de agregarRecursos que quedo de antes

--- src/main/java/edu/fiuba/algo3/vistas/App.java
+++ src/main/java/edu/fiuba/algo3/vistas/App.java
@@ -32,33 +32,6 @@ public class App extends Application {
         root.setStyle("-fx-background-color: #87cfe8;");
 
         Juego juego = new Juego(jugadores);
-        
-
-      // ==================== HARCODEO PARA PROBAR ==========================
-     //JUgador 1 con madera y puerto especifico de madera
-        Jugador jugadorTest = juego.jugadorActual();
-        jugadorTest.recibirRecurso(new Madera(0), 20);
-        Puerto puertoMadera = new PuertoEspecifico(new Madera(0), new UbicacionVertice(1), new UbicacionVertice(2));
-
-        jugadorTest.getPuertos().add(puertoMadera);
-
-        System.out.println(">>> HACK ACTIVADO: " + jugadorTest.nombre() + " tiene 20 Maderas y un Puerto de Madera (2:1)");
-       
-        
-        
-     //jugador dos con ladrillos 
-        Jugador jugador1 = juego.jugadorActual();
-        jugador1.recibirRecurso(new Madera(0), 20);
-        jugador1.getPuertos().add(new PuertoEspecifico(new Madera(0), new UbicacionVertice(1), new UbicacionVertice(2)));
-
-        if (jugadores.size() > 1) {
-            Jugador jugador2 = jugadores.get(1);
-            jugador2.recibirRecurso(new Ladrillo(0), 20); 
-            
-            System.out.println(">>> HACK: Jugador 2 (" + jugador2.nombre() + ") cargado con 20 Ladrillos.");
-        }
-
-        // ============================================================
 
         JuegoObservable modeloObservable = new JuegoObservable(juego);
         VistaJuego vistaJuego = new VistaJuego(modeloObservable);
